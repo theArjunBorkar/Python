95ec449d59a19ddd715130cc752d9379b52feec5
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-23 15:48:51 +0530

    Update common list elements.py

--- common list elements.py
+++ common list elements.py
@@ -4,4 +4,4 @@ print("Common list elements: ")
 for i in range(len(lst_A)):
     for j in range(len(lst_B)):
         if lst_A[i] == lst_B[j]:
-            print(lst_A[i], "")
+            print(lst_A[i])
