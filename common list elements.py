03e554f1be732a972f18a194ee979e12710bb307
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-23 15:47:39 +0530

    common list elements

---
+++ common list elements.py
@@ -0,0 +1,7 @@
+lst_A = eval(input("Enter list A elements: "))
+lst_B = eval(input("Enter list B elemens: "))
+print("Common list elements: ")
+for i in range(len(lst_A)):
+    for j in range(len(lst_B)):
+        if lst_A[i] == lst_B[j]:
+            print(lst_A[i], "")
