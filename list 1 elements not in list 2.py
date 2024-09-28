6c89c54c194b68e993eb66a905ff52b67e5f1d26
Arjun Borkar <arjunborkar2006@gmail.com>
2024-09-28 11:34:04 +0530

    Create list 1 elements not in list 2.py

---
+++ list 1 elements not in list 2.py
@@ -0,0 +1,9 @@
+def unique_elements(list_1, list_2):
+    unique_list = []
+    for i in range(len(list_1)):
+        if list_1[i] not in list_2[i]:
+            unique_list.append(list_1[i])
+    return unique_list
+list_1 = eval(input("Enter list 1 elements: "))
+list_2 = eval(input("Enter list 2 elements: "))
+print("Elements that are in list 1 but not in list 2 are:", unique_elements(list_1, list_2))
